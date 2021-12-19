e23f6287e9746056171977c0b497edf98b3fd5b1
Nicholas Oxford <51415676+nicholasoxford@users.noreply.github.com>
2021-12-19 18:57:41 -0500

    day1

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,14 @@
-import { Button } from "ui";
+import Link from "next/link";
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1>100 Days</h1>
+      <ul>
+        <li>
+          <Link href="/1">Day One</Link>
+        </li>
+      </ul>
     </div>
   );
 }
