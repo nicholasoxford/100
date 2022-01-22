2f6ebbd6d78bdeb03a8da53cf31c121b16387afd
Nicholas Oxford <noxford@Nicholass-MacBook-Pro.local>
2022-01-22 12:28:33 -0500

    removingTailWinds

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,30 +1,19 @@
 import Link from 'next/link'
 import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
+import { days } from '../public/days'
 export default function Page() {
-
-
-    
-
   return (
     <div>
-      <ul>
-        <li>
-          <Link href="/100days/1">Day One</Link>
-        </li>
-        <li>
-          <Link href="/100days/2">Day Two</Link>
-        </li>
-        <li>
-          <Link href="/100days/3">Day Three</Link>
-        </li>
-        <li>
-          <Link href="/100days/4">Day Four</Link>
-        </li>
-        <li>
-          <Link href="/100days/5">Day Five</Link>
-        </li>
+      <ul style={{ 
+        listStyle: 'none',
+      }}>
+        {days.map(day => (
+          <li key={day.day}>
+            <Link href={`/100days/${day.day}`} passHref >
+             <a className='text-xl'> Day {day.day}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   )
