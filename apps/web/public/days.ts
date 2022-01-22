d9d5457d960988b65a54004c65482074c28c711a
Nicholas Oxford <maheledaily@gmail.com>
2022-01-22 12:29:11 -0500

    removingTailWinds

---
+++ apps/web/public/days.ts
@@ -0,0 +1,32 @@
+import { infoProps } from 'ui/components/info'
+
+export const days: infoProps[] = [
+  {
+    day: 1,
+    description: 'Learning to use Matter - a la Antfu',
+    goal: 'Get a Turbo monorepo set up and deploy to Vercel.'
+  },
+  {
+    day: 2,
+    description:
+      "Visualizing Some Maths. Although this is a very basic visualisation of a sin wave, it's something I've wanted to do for awhile.",
+    goal: 'I created another workspace in my repo and installed it as a dependency.'
+  },
+  {
+    day: 3,
+    description: 'Broke out a lot of the hardwork into functions. You can also move the camera around.',
+    goal: 'Continue to learn ThreeJS and vizualize tan, cos, and sin functions.'
+  },
+  {
+    day: 4,
+    description:
+      'Less impressive day on the threejs front, but I got layout working and had a few failed attempts at vizualization.',
+    goal: 'Onward and upward.'
+  },
+  {
+    day: 5,
+    description:
+      'I created a chrome extension that parses certain websites for longitude and latitude coordinates. Land watch then sends the long, lat, and zipcode to my api which returns a list of closest trader joes, air quality, and plant hardiness.',
+    goal: 'Create a chrome extension.'
+  }
+]
